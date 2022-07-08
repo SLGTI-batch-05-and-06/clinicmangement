19ed44f84dbb431ee6664d28a1f7569e24ed339f
Thanujakanthan <thanuja.thanujalingam@gmail.com>
2022-07-07 20:09:30 -0700

    first

--- for.php
+++ for.php
@@ -8,11 +8,30 @@ include_once('script.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>forgot</title>
-</head>
-<body>
-    
 <link rel="stylesheet" href="for.css">
   <!-- Add icon library -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<img src="thanu2-removebg-preview.png" class="logo" ><p><b>PEOPLE CARE<BR>CLINIC</b></P>
+<div class="container"> <br>
+   <div class="header">
+      <h1>FORGOT  <br>  PASSWORD</h1>
+</div> <br> <br> <br> <br> <br>
+<div class="main">
+    <p> Enter your E-Mail below </p>
+     <form>
+        <span>
+          <i class=""></i>
+          <input type="email" placeholder="E-Mail" name="mail">
+        </span><br>
+        
+        <span>
+          <i class=""></i>
+          <input type="password" placeholder="New password" name="">
+
+       </span> 
+            <button  type="submit">UPDATE</button> <br>
+            <span>
 </body>
 </html>
